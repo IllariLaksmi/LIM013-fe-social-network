34a6171d0052cf811a0374aacad3c672752b2adc
Illari Alvarez-Gil <friedapples824qgmail.com>
2020-11-13 20:15:12 -0500

    cambios

--- src/lib/firebase/auth.js
+++
@@ -1,6 +0,0 @@
-//Creación de nueva cuenta de usuario
-//import firebase from 'firebase';
-
-export const signUpUser = (email, password) => {
-  return firebase.auth().createUserWithEmailAndPassword(email, password);
-};
--- src/lib/firebase/firestore.js
+++
@@ -1,9 +0,0 @@
-//Uso de base de datos Firestore
-export const  createUser = (userUid, email, name, photo) => { firebase.firestore()
-    .collection("usuarios").add({
-        name,
-        email,
-        userUid,
-        photo
-    })
-}
